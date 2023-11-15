87e4d941501c4b7bb1ed8dd0d7a4d14e11a36db4
d.shkinin <amiddio.dev@gmail.com>
2023-11-15 10:51:48 +0400

    Added url pattern and view

--- lidhit_test_task/app/views.py
+++ lidhit_test_task/app/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from .services.form_checker_service import FormCheckerService
+from .services.tinydb_data_source import TinydbDataSource
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class FormCheckerView(View):
+    """
+    Представление обрабатывающее POST запросы
+    """
+
+    def post(self, request):
+        service = FormCheckerService(
+            data_source=TinydbDataSource(data=request.POST)
+        )
+        res = service.search()
+
+        if isinstance(res, dict):
+            return JsonResponse(res)
+
+        return HttpResponse(res)
--- lidhit_test_task/lidhit_test_task/settings.py
+++ lidhit_test_task/lidhit_test_task/settings.py
@@ -39,12 +39,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Apps
+
+    # Local Apps
+    'app.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DB_NAME = 'db.json'
